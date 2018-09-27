aa24d66244c33ba5083125a3752563a7710bc366
HaedongKim <haedoang@gmail.com>
2018-09-27 17:43:39 +0900

    1234

--- team5/src/main/java/kr/co/fishbang/repository/domain/ProfileDB.java
+++ team5/src/main/java/kr/co/fishbang/repository/domain/ProfileDB.java
@@ -1,12 +1,13 @@
 package kr.co.fishbang.repository.domain;
 
-public class Profile {
+public class ProfileDB {
 	
 	//프로필 사진 vo
-	private int picNo;
+	private int no;		//프로필DB
 	private String userId;
 	private String oriName;
-	private String sysName;
+	private String sysProfileName;
+	private String sysThumbnailName;
 	private String path;
 	
 	
