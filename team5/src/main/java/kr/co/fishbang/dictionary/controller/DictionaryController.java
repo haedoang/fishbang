1e3df4385b7ee9fd661030a226a02802c7de0318
kim <rlaqudrhks258@naver.com>
2018-10-01 09:48:38 +0900

    Revert "Revert "dictionaryall, popup, fishdictInfo, DictionaryController""
    This reverts commit ce6e2906e9873928bc3d71db6026ff9087132b6c.

--- team5/src/main/java/kr/co/fishbang/dictionary/controller/DictionaryController.java
+++ team5/src/main/java/kr/co/fishbang/dictionary/controller/DictionaryController.java
@@ -23,7 +23,7 @@ public class DictionaryController extends HttpServlet {
 		DictionaryMapper mapper = 
 				MyAppSqlConfig.getSqlSessionInstance().getMapper(DictionaryMapper.class);
 
-		int no = 1;
+		int no = Integer.parseInt(request.getParameter("m"));
 		switch(no) {
 		
 		case 1: List<Dictionary> dict1 =  mapper.selectDictionary1();
@@ -32,7 +32,7 @@ public class DictionaryController extends HttpServlet {
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd1.forward(request, response); break;
+		rd1.forward(request, response); return;
 		
 		case 2 : List<Dictionary> dict2 =  mapper.selectDictionary2();
 		request.setAttribute("dict", dict2);
@@ -40,7 +40,7 @@ public class DictionaryController extends HttpServlet {
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd2.forward(request, response);break;
+		rd2.forward(request, response);return;
 		case 3 : List<Dictionary> dict3 =  mapper.selectDictionary3();
 		request.setAttribute("dict", dict3);
 		RequestDispatcher rd3 = request.getRequestDispatcher(
@@ -55,13 +55,14 @@ public class DictionaryController extends HttpServlet {
 				);
 		
 		rd4.forward(request, response);break;
+		
 		case 5 : List<Dictionary> dict5 =  mapper.selectDictionary5();
 		request.setAttribute("dict", dict5);
 		RequestDispatcher rd5 = request.getRequestDispatcher(
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd5.forward(request, response);
+		rd5.forward(request, response); break;
 		
 		case 6 : List<Dictionary> dict6 =  mapper.selectDictionary6();
 		request.setAttribute("dict", dict6);
@@ -69,42 +70,42 @@ public class DictionaryController extends HttpServlet {
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd6.forward(request, response);
+		rd6.forward(request, response);break;
 		case 7 : List<Dictionary> dict7 =  mapper.selectDictionary7();
 		request.setAttribute("dict", dict7);
 		RequestDispatcher rd7 = request.getRequestDispatcher(
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd7.forward(request, response);
+		rd7.forward(request, response);return;
 		case 8 : List<Dictionary> dict8 =  mapper.selectDictionary8();
 		request.setAttribute("dict", dict8);
 		RequestDispatcher rd8 = request.getRequestDispatcher(
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd8.forward(request, response);
+		rd8.forward(request, response);return;
 		case 9 : List<Dictionary> dict9 =  mapper.selectDictionary9();
 		request.setAttribute("dict", dict9);
 		RequestDispatcher rd9 = request.getRequestDispatcher(
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd9.forward(request, response);
+		rd9.forward(request, response);return;
 		case 10 : List<Dictionary> dict10 =  mapper.selectDictionary10();
 		request.setAttribute("dict", dict10);
 		RequestDispatcher rd10 = request.getRequestDispatcher(
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd10.forward(request, response);break;
+		rd10.forward(request, response);return;
 		case 11 : List<Dictionary> dict11 =  mapper.selectDictionary11();
 		request.setAttribute("dict", dict11);
 		RequestDispatcher rd11 = request.getRequestDispatcher(
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd11.forward(request, response);break;
+		rd11.forward(request, response);return;
 		
 	
 		case 12 : List<Dictionary> dict12 =  mapper.selectDictionary12();
@@ -113,77 +114,77 @@ public class DictionaryController extends HttpServlet {
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd12.forward(request, response);break;
+		rd12.forward(request, response);return;
 		case 13 : List<Dictionary> dict13 =  mapper.selectDictionary13();
 		request.setAttribute("dict", dict13);
 		RequestDispatcher rd13 = request.getRequestDispatcher(
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd13.forward(request, response);break;
+		rd13.forward(request, response);return;
 		case 14 : List<Dictionary> dict14 =  mapper.selectDictionary14();
 		request.setAttribute("dict", dict14);
 		RequestDispatcher rd14 = request.getRequestDispatcher(
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd14.forward(request, response);break;
+		rd14.forward(request, response);return;
 		case 15 : List<Dictionary> dict15 =  mapper.selectDictionary15();
 		request.setAttribute("dict", dict15);
 		RequestDispatcher rd15 = request.getRequestDispatcher(
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd15.forward(request, response);break;
+		rd15.forward(request, response);return;
 		case 16 : List<Dictionary> dict16 =  mapper.selectDictionary16();
 		request.setAttribute("dict", dict16);
 		RequestDispatcher rd16 = request.getRequestDispatcher(
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd16.forward(request, response);break;
+		rd16.forward(request, response);return;
 		case 17 : List<Dictionary> dict17 =  mapper.selectDictionary17();
 		request.setAttribute("dict", dict17);
 		RequestDispatcher rd17 = request.getRequestDispatcher(
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd17.forward(request, response);break;
+		rd17.forward(request, response);return;
 		case 18 : List<Dictionary> dict18 =  mapper.selectDictionary18();
 		request.setAttribute("dict", dict18);
 		RequestDispatcher rd18 = request.getRequestDispatcher(
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd18.forward(request, response);break;
+		rd18.forward(request, response);return;
 		case 19 : List<Dictionary> dict19 =  mapper.selectDictionary19();
 		request.setAttribute("dict", dict19);
 		RequestDispatcher rd19 = request.getRequestDispatcher(
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd19.forward(request, response);break;
+		rd19.forward(request, response);return;
 		case 20 : List<Dictionary> dict20=  mapper.selectDictionary20();
 		request.setAttribute("dict", dict20);
 		RequestDispatcher rd20 = request.getRequestDispatcher(
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd20.forward(request, response);break;
+		rd20.forward(request, response);return;
 		case 21 : List<Dictionary> dict21 =  mapper.selectDictionary21();
 		request.setAttribute("dict", dict21);
 		RequestDispatcher rd21 = request.getRequestDispatcher(
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd21.forward(request, response);break;
+		rd21.forward(request, response);return;
 		case 22 : List<Dictionary> dict22 =  mapper.selectDictionary22();
 		request.setAttribute("dict", dict22);
 		RequestDispatcher rd22 = request.getRequestDispatcher(
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd22.forward(request, response);break;
+		rd22.forward(request, response);return;
 		
 		case 23 : List<Dictionary> dict23 =  mapper.selectDictionary23();
 		request.setAttribute("dict", dict23);
@@ -191,7 +192,7 @@ public class DictionaryController extends HttpServlet {
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd23.forward(request, response);break;
+		rd23.forward(request, response);return;
 		
 		case 24 : List<Dictionary> dict24 =  mapper.selectDictionary24();
 		request.setAttribute("dict", dict24);
@@ -199,7 +200,7 @@ public class DictionaryController extends HttpServlet {
 				"/jsp/dictionary/fishDictAll.jsp"
 				);
 		
-		rd24.forward(request, response);break;
+		rd24.forward(request, response);return;
 	}
 }
 }
